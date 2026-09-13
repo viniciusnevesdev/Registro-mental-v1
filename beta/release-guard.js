c06f8e013676e9a6e92b65f42ddbdd6d24234432
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 09:31:03 -0300

    Beta 44: centraliza título do lembrete

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.43 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.43';
+/* Registro Mental release 1.2.0-beta.44 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.44';
