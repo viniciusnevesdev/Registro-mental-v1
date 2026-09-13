be404437a1c9ce2422caff6d1281e47a5d3f17fc
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 10:56:02 -0300

    Adicionar monitor de tempo acordado na Beta

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.44 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.44';
+/* Registro Mental release 1.2.0-beta.45 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.45';
