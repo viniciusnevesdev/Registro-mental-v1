2c6ebfaed4b0f9ba3b21613bb66436c0e2e76bf3
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 08:30:11 -0300

    Beta 41: remove caixa dos ícones de continuidade

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.40 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.40';
+/* Registro Mental release 1.2.0-beta.41 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.41';
