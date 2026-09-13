6dfcd270163ec5317604193b285860ef3a81f7e1
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 09:06:56 -0300

    Beta 42: reorganiza cabeçalho do lembrete

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.41 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.41';
+/* Registro Mental release 1.2.0-beta.42 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.42';
