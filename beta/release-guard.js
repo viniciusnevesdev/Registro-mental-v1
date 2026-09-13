a3328fe27ab277d712b1a75f17128335d9c0a6f5
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 09:29:33 -0300

    Beta 43: aumenta título do lembrete

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.42 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.42';
+/* Registro Mental release 1.2.0-beta.43 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.43';
