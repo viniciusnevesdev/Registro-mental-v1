cc2c1def01b457122e9a9db95da7592790c4617a
Vinicius <viniciusnevez123@gmail.com>
2026-09-15 03:59:55 -0300

    Oficial 1.2.4: força atualização após correção do boot

--- boot.js
+++ boot.js
@@ -2,7 +2,7 @@
 (() => {
   'use strict';
 
-  const RELEASE = '1.2.3';
+  const RELEASE = '1.2.4';
   const STARTED = performance.now();
   const scopeToken = '/Registro-mental-v1/';
   const DIAG_KEY = 'registro-last-diagnostic-v1';
